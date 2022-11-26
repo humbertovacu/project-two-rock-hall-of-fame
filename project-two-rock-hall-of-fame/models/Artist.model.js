42c32d68998f0e073924e0186e20c2ef543bff1f
Arcadio REYES <arcadioreyesph@gmail.com>
2022-11-26 14:30:00 +0100

    Artidt.model.js created

---
+++ project-two-rock-hall-of-fame/models/Artist.model.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+
+const artistSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+    required: true,
+  },
+  origin: {
+    type: String,
+    required: true,
+  },
+  birthday: {
+    type: String,
+    required: true,
+  },
+  bands: {
+    type: [String],
+    required: true,
+  },
+  instrument: {
+    type: [String],
+    required: true,
+  },
+  genres: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+});
+
+const Artist = model("Artist", artistSchema);
+
+module.exports = Artist;
