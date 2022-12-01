0ef62bdb8f3b6efa8c6e0ca040e5aa7d5b773cfc
Arcadio REYES <arcadioreyesph@gmail.com>
2022-12-01 21:09:40 +0100

    list of bands and photos added to bands.hbs

--- project-two-rock-hall-of-fame/routes/band.routes.js
+++ project-two-rock-hall-of-fame/routes/band.routes.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const Band = require("../models/Band.model");
 const router = express.Router();
 
 router.get("/new-band", (req, res) => {
   res.render("create-band");
 });
 
-router.get("/", (req, res) => {
-  res.render("bands");
+router.get("/", (req, res, next) => {
+  Band.find()
+    .then((allTheBandsFromDB) => {
+      console.log("Retrieved bands from DB:", allTheBandsFromDB);
+      // we call the render method after we obtain the bands data from the database -> allTheBandsFromDB
+      res.render("bands.hbs", { bands: allTheBandsFromDB }); // pass `allTheBandsFromDB` to the view (as a variable bands to be used in the HBS)
+    })
+
+    .catch((error) => {
+      console.log("Error while getting the bands from the DB: ", error);
+      // Call the error-middleware to display the error page to the user
+      next(error);
+    });
 });
 
 module.exports = router;
