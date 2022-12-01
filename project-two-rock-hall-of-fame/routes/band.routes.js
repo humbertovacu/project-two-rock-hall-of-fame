a68056cb5fe8b40d40ee433d53b02374d3938435
Arcadio REYES <arcadioreyesph@gmail.com>
2022-12-01 14:43:35 +0100

    tried to add a route to the bands at band.roues.js , not working so I commented out

--- project-two-rock-hall-of-fame/routes/band.routes.js
+++ project-two-rock-hall-of-fame/routes/band.routes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+// const app = express();
 
 router.get("/new-band", (req, res) => {
   res.render("create-band");
 });
 
+// app.get("/bands", (req, res) => {
+//   res.render("bands");
+// });
+
 module.exports = router;
+// module.exports = app;
