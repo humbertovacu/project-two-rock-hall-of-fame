797787efd92b0834a50c7db89b20cf5236e4d79d
humbertovacu <humbertovacu@gmail.com>
2022-12-13 18:45:14 +0100

    moved repository

--- project-two-rock-hall-of-fame/public/js/create-artist-script.js
+++
@@ -1,8 +0,0 @@
-// document.getElementById('add-band').addEventListener('click', () => {
-//     const bandLabel = document.getElementById("add-band-label");
-//     const newBandInput = document.createElement('input');
-//     newBandInput.setAttribute('type', 'text')
-//     newBandInput.setAttribute('name', 'bands')
-//     bandLabel.appendChild(newBandInput)
-// });
-
