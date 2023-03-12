f568d17a94f455a561f743ead116b2a04045907e
Humberto Vallejo <111450057+humbertovacu@users.noreply.github.com>
2023-03-12 19:17:33 +0100

    :bug: sign up route bug fix

--- routes/signup.routes.js
+++ routes/signup.routes.js
@@ -5,27 +5,23 @@ const saltRounds = 10;
 const User = require("../models/User.model");
 const { userLoggedIn, userLoggedOut } = require("../middleware/route-guard.js");
 
-/////////SIGNUP/////////
+
 router.get("/", userLoggedOut, (req, res) => {
   res.render("auth/sign-up");
 });
 
-/* POST Signup page */
+
 router.post("/", (req, res, next) => {
-  console.log("The form data: ", req.body);
 
   const { username, email, password } = req.body;
 
-  // here follow 3 backend validators:
-
-  // validation 1:  check if we have all info
   if (!username || !email || !password) {
     res.render("auth/sign-up", {
       errorMessage: "please fill out all information",
     });
     return;
   }
-  // validation 2: Check email format, with a regex
+  
   const emailRegex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -33,9 +29,8 @@ router.post("/", (req, res, next) => {
     res.render("auth/sign-up", {
       errorMessage: "Please present a valid email",
     });
-    return;
-  }
-  // validation 3: check if username is unique
+    return};
+
   User.findOne({ username })
     .then((user) => {
       if (user) {
@@ -43,7 +38,6 @@ router.post("/", (req, res, next) => {
           errorMessage: "that user is already registered",
         });
       } else {
-        //encrypt password and create the user in the DB
         bcryptjs
           .genSalt(saltRounds)
           .then((salt) => bcryptjs.hash(password, salt))
